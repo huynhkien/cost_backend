86ad32e0b5352fd60e5b8a3df89e16d15bed3475
huynhkien <huynhhuukien2962@gmail.com>
2025-08-29 16:43:21 +0700

    Cofig for backend

---
+++ src/config/index.js
@@ -0,0 +1,22 @@
+const index = {
+    db_server: {
+        key: process.env.DB_SERVER
+    },
+    db_port: {
+        key: process.env.DB_PORT
+    },
+    db_name: {
+        key: process.env.DB_NAME
+    },
+    db_login: {
+        key: process.env.DB_LOGIN
+    },
+    db_password: {
+        key: process.env.DB_PASSWORD
+    },
+    port: {
+        key: process.env.PORT
+    }
+
+}
+module.exports = index
