d3dd4e87bc3f5f5e46c7692cfa14061f56f85c61
huynhkien <huynhhuukien2962@gmail.com>
2025-09-03 16:53:37 +0700

    Update file app.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const dmCU2Router = require('./src/routes/dmCU2Route')
-const qltkRouter = require('./src/routes/qltkRoute')
+const dmCU2Router = require('./src/routes/dmCU2Route');
+const dmS1Router = require('./src/routes/dmS1Route');
+const qltkRouter = require('./src/routes/qltkRoute');
+const giaCU2Router = require('./src/routes/giaCU2Route');
+const giaS1Router = require('./src/routes/giaS1Route');
 const app = express()
 // cors
 app.use(cors())
@@ -12,6 +15,9 @@ app.get('/', (req, res) => {
         message: 'Server đang chạy'
     });
 });
-app.use('/api/dmCU2',dmCU2Router)
-app.use('/api/qltk',qltkRouter)
+app.use('/api/dmCU2',dmCU2Router);
+app.use('/api/dmS1',dmS1Router);
+app.use('/api/giaCU2', giaCU2Router);
+app.use('/api/giaS1', giaS1Router);
+app.use('/api/qltk',qltkRouter);
 module.exports = app
