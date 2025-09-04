77718dee3e82ebe5842c1eae24b05dd69667923d
huynhkien <huynhhuukien2962@gmail.com>
2025-09-04 17:04:10 +0700

    Add file dmRoute.js

---
+++ src/routes/dmRoute.js
@@ -0,0 +1,10 @@
+const dmControllers = require('../controllers/dmController');
+const express = require('express');
+const router = express.Router();
+
+router.route('/nvl').get(dmControllers.get_dsNVL);
+router.route('/vtdg').get(dmControllers.get_dsVTDG);
+router.route('/ct').get(dmControllers.get_dsCONGTHUC);
+
+
+module.exports = router;
