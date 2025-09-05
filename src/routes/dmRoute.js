de76f880af90946b48b5be37bfcea035883e7990
huynhkien <huynhhuukien2962@gmail.com>
2025-09-05 16:38:25 +0700

    Update file dmRoute.js

--- src/routes/dmRoute.js
+++ src/routes/dmRoute.js
@@ -6,5 +6,7 @@ router.route('/nvl').get(dmControllers.get_dsNVL);
 router.route('/vtdg').get(dmControllers.get_dsVTDG);
 router.route('/ct').get(dmControllers.get_dsCONGTHUC);
 
+router.route('/detail-ct').get(dmControllers.get_dsCTDetail);
+
 
 module.exports = router;
