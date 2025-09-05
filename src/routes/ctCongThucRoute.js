96097459d8ff5c93e40782f4a230b868a6b7d964
huynhkien <huynhhuukien2962@gmail.com>
2025-09-05 13:30:31 +0700

    Delete file

--- src/routes/ctCongThucRoute.js
+++
@@ -1,8 +0,0 @@
-const ctCongThucControllers = require('../controllers/ctCongThucController');
-const express = require('express');
-const router = express.Router();
-
-router.route('/').get(ctCongThucControllers.get_ctCongThuc);
-
-
-module.exports = router;
